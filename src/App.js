a6a82cfc609ca1cd1a92dc61e79eddb484c9e08b
Emma Duffy <emma.duffy@mycit.ie>
2024-06-09 23:32:58 +0100

    change server url

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import io from 'socket.io-client';
 import { useState } from 'react';
 import Chat from './Chat';
 
-const socket = io.connect('http://localhost:3000');
+const socket = io.connect('https://chat-app-vrfc.onrender.com/');
 
 function App() {
   const [username, setUsername] = useState("");
